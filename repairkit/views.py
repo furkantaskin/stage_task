30d700dcee23f1668b2195063aab306bb63f749e
Furkan Taşkın <furkan.taskin@tuta.io>
2023-05-24 20:57:20 +0300

    Add function for form operation to add new car

--- repairkit/views.py
+++ repairkit/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Car, Operation
 
-# Create your views here.
+
+def home(request):
+    cars = Car.objects.all()
+    operations = Operation.objects.all()
+    return render(request, 'repairkit/home.html', {'cars': cars, 'operations': operations})
+
+
+def cars(request):
+    cars = Car.objects.all()
+    return render(request, 'repairkit/cars.html', {'cars': cars})
+
+
+def add_car(request):
+    if request.method == "POST":
+        brand = request.POST.get("brand")
+        model = request.POST.get("model")
+        year = request.POST.get("year")
+        color = request.POST.get("color")
+        plate = request.POST.get("plate")
+
+        print(f"Model adı: {model}")
+        car = Car(brand=brand, model=model, plate_number=plate, color=color, year=year)
+        car.save()
+        return redirect("home")
+    else:
+        return render(request, 'repairkit/add_car.html')
