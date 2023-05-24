56e542b4c3c6cd924b02acad3ae6d838c2e3b713
Furkan Taşkın <furkan.taskin@tuta.io>
2023-05-24 19:53:58 +0300

    Initial commit

---
+++ repairkit/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RepairkitConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'repairkit'
---
+++ repairkit/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+import datetime
+from uuid import uuid4
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=20)
+    model = models.CharField(max_length=40)
+    plate_number = models.CharField(max_length=10, unique=True)
+    year = models.PositiveIntegerField(default=datetime.date.today().year)
+    color = models.CharField()
+
+    def __str__(self):
+        return self.plate_number
+
+
+class Operations(models.Model):
+    __operation_code = f"{datetime.date.today().strftime('%Y%m%d')}-{uuid4()}"
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    operation_date = models.DateField(default=datetime.date.today())
+    description = models.TextField()
+    operation_code = models.TextField(default=__operation_code)
+
+    def __str__(self):
+        return f"{self.car.plate_number} - {self.operation_date}"
