7a9cebc0f58abfb08f978e8a6f195eb9a2b668e8
Furkan Taşkın <furkan.taskin@tuta.io>
2023-05-25 19:16:08 +0300

    Creat url rules for operations

--- repairkit/urls.py
+++ repairkit/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import add_car, home, cars
+from .views import add_car, home, cars, operations, add_operation, delete_operation, edit_operation
 
 urlpatterns = [
     path('add_car', add_car, name='add_car'),
     path('home', home, name="home"),
-    path('cars', cars, name="cars")
+    path('cars', cars, name="cars"),
+    path('operations/<int:car_id>', operations, name="operations"),
+    path('add_operation', add_operation, name="add_operation"),
+    path('delete_operation/<int:operation_id>', delete_operation, name="delete_operation"),
+    path('edit_operation/<int:operation_id>', edit_operation, name="edit_operation"),
 ]
